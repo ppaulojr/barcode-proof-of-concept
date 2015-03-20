488214a3d02bdc4d8472c8cf3722220cec6b5e4f
Pedro Paulo <ppj@netfilter.com.br>
2015-03-20 14:57:46 -0300

    Initial Commit

---
+++ barcode-proof-of-concept/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  barcode-proof-of-concept
+//
+//  Created by Pedro Paulo Oliveira Junior on 20/03/15.
+//  Copyright (c) 2015 speedcomm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ barcode-proof-of-conceptTests/barcode_proof_of_conceptTests.m
@@ -0,0 +1,40 @@
+//
+//  barcode_proof_of_conceptTests.m
+//  barcode-proof-of-conceptTests
+//
+//  Created by Pedro Paulo Oliveira Junior on 20/03/15.
+//  Copyright (c) 2015 speedcomm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface barcode_proof_of_conceptTests : XCTestCase
+
+@end
+
+@implementation barcode_proof_of_conceptTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
